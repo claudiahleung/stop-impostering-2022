91a2eb8226aa20e3f67f44094b657eee15119ef8
Claudia Leung <claudiahleung@gmail.com>
2022-04-26 00:18:35 +0100

    started 5.1, need to review quicksort

---
+++ 5-arrays/5.1.py
@@ -0,0 +1,16 @@
+"""
+the dutch national flag problem
+
+"""
+
+
+def main(array, index):
+
+return
+
+if "__name__" == "__main__":
+
+array = [0, 1, 2, 0, 2, 1, 1]
+index = array[3]
+
+main(array, index)
